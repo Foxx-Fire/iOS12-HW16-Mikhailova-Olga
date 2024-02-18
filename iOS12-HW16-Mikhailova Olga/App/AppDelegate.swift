a618c3a724eeba65259895cb21b6ff4b19e318e3
Olga Mikhailova <foxx_fire@mail.ru>
2024-02-19 00:36:20 +0300

    Structure

---
+++ iOS12-HW16-Mikhailova Olga/App/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  iOS12-HW16-Mikhailova Olga
+//
+//  Created by mbpro2.0/16/512 on 19.02.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate { }
